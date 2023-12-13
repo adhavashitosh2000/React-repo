ffa10c020789816b1e7c7ae07924e8caf6e0c4db
demoRapo <adhavashitosh373@gmail.com>
2023-12-13 23:38:28 +0530

    Valid mail and password then Show

--- src/componunt/Login.jsx
+++ src/componunt/Login.jsx
@@ -1,15 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [AllInfo, setAllInfo] = useState([]);
 
+  useEffect(()=>{
+    
+  },[])
   const handleLoginClick = () => {
     if (email && password) {
       setAllInfo([...AllInfo, { email, password }]);
-      setEmail("")
-      setPassword("")
+      setEmail("");
+      setPassword("");
     } else {
       alert("Add valid email and password");
     }
@@ -19,7 +22,8 @@ function Login() {
     <>
       <form>
         <div className="container">
-          <input  className="input"
+          <input
+            className="input"
             type="email"
             placeholder="email"
             value={email}
@@ -27,7 +31,8 @@ function Login() {
           />
         </div>
         <div className="container">
-          <input className="input"
+          <input
+            className="input"
             type="password"
             placeholder="password"
             value={password}
@@ -35,15 +40,16 @@ function Login() {
           />
         </div>
       </form>
-      <button className="Submit" onClick={handleLoginClick}>
+      <button className="submit" onClick={handleLoginClick}>
         Submit
       </button>
       <div>
-        {AllInfo.map((item, index) => (
-          <h2 key={index}>
-            {item.email} - {item.password}
+        {AllInfo.length > 0 ? AllInfo.map((item) => (
+          <h2 key={item.email + item.password}>
+            {item.email} - {item.password} 
           </h2>
-        ))}
+        )): <h2> not give you eamil and password</h2>}
+        
       </div>
     </>
   );
