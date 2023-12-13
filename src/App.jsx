17becae03c411b5a5636be3bad512f653fa8bbc5
demoRapo <adhavashitosh373@gmail.com>
2023-12-13 19:14:48 +0530

    Add All Files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Login from './componunt/Login'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>
+      Hello World
+      </div>
+      <Login />
+      </>
+  )
+}
+
+export default App
---
+++ src/componunt/Login.jsx
@@ -0,0 +1,10 @@
+
+function Login() {
+  return (
+    <div>
+      <input type="text" placeholder='email'/>
+    </div>
+  )
+}
+
+export default Login
